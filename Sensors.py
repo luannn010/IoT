05827d968ea349eab02d364477612c34d0ce36e6
Luan Nguyen <luannn010@gmail.com>
2024-02-06 22:07:21 +1100

    p

--- Sensors.py
+++ Sensors.py
@@ -2,32 +2,39 @@ import logging.handlers
 import os
 from tb_gateway_mqtt import TBDeviceMqttClient
 import serial
-import credentials
+import credentials # Import the credentials file to access the access token and ThingsBoard server details
 
 ACCESS_TOKEN = credentials.ACCESS_TOKEN
 THINGSBOARD_SERVER = credentials.THINGSBOARD_SERVER
 THINGSBOARD_PORT = credentials.THINGSBOARD_PORT
+    
+ip_address = credentials.get_network_info()[0]
+mac_address = credentials.get_network_info()[1] 
 
 logging.basicConfig(level=logging.DEBUG)
 client = None
 ser = serial.Serial(
-    port='COM6', # Change port
-    baudrate=115200,
+    port='COM6',
+    baudrate=9600,
     parity="N",
     stopbits=1,
     bytesize=8
 )
-ser.isOpen();
+ser.isOpen()
 
 # default blinking period
 period = 1.0
 
 # Read the serial for any message. If a message is received, read it byte-by-byte and return it.
 def readSerial():
-    out = None
+    out = b''  # Initialize an empty byte array
     while ser.in_waiting > 0:
-        out = ser.read()
-    return out
+        byte = ser.read()
+        out += byte
+        if byte == b'\n':  # Assuming messages end with a newline character
+            return out.strip()  # Strip any leading/trailing whitespaces
+
+    return None
 
 # callback function that will call when we will send RPC
 def rpc_callback(id, request_body):
@@ -35,7 +42,7 @@ def rpc_callback(id, request_body):
     print(request_body)
     method = request_body.get('method')
     if method == 'getTelemetry':
-        attributes, telemetry = prepareData()
+        attributes, telemetry = prepareData(ser)
         client.send_attributes(attributes)
         client.send_telemetry(telemetry)
     else:
@@ -44,25 +51,23 @@ def rpc_callback(id, request_body):
 # Prepares data to be sent to via MQTT to the ThingsBoard platform.
 # Data is formatted as a JSON string.
 # This implementation currently only takes a singly byte as the message, which is passed into the motion_active variable.
-def prepareData(msg):
-    # Assuming the message format is "flame:<flame_value>,smoke:<smoke_value>"
-    data = msg.decode("utf-8").split(',')
-    flame_value = data[0].split(':')[1]
-    smoke_value = data[1].split(':')[1]
+def prepareData(data):
+    
+    if data is not None:
+        flame_value, smoke_value = data.split(",")
 
-    ip_address = os.popen('''hostname -I''').readline().replace('\n', '').replace(',', '.')[:-1]
-    mac_address = os.popen('''cat /sys/class/net/*/address''').readline().replace('\n', '').replace(',', '.')
-
-    attributes = {
-        'ip_address': ip_address,
-        'macaddress': mac_address
-    }
-    telemetry = {
-        'flame_value': int(flame_value),
-        'smoke_value': int(smoke_value)
-    }
-    print(attributes, telemetry)
-    return attributes, telemetry
+        attributes = {
+            'ip_address': ip_address,
+            'macaddress': mac_address
+        }
+        telemetry = {
+            'flame_value': int(flame_value),
+            'smoke_value': int(smoke_value)
+        }
+        print(attributes, telemetry)
+        return attributes, telemetry
+    else:
+        return None, None
 
 # request attribute callback
 def sync_state(result, exception=None):
@@ -77,17 +82,16 @@ def main():
     client = TBDeviceMqttClient(THINGSBOARD_SERVER, THINGSBOARD_PORT, ACCESS_TOKEN)
     client.connect()
     client.request_attributes(shared_keys=['blinkingPeriod'], callback=sync_state)
-
+    
     # now rpc_callback will process rpc requests from server
     client.set_server_side_rpc_request_handler(rpc_callback)
 
     while not client.stopped:
-        # Only send data to the ThingsBoard if a message is sent via Serial.
-        msg = readSerial()
-        if (msg != None):
-            attributes, telemetry = prepareData(msg)
+        data = readSerial()
+        if data is not None:
+            data = data.decode()
+            attributes, telemetry = prepareData(data)
             client.send_attributes(attributes)
-            client.send_telemetry(telemetry)
-
-if __name__=='__main__':
+            client.send_telemetry(telemetry)      
+if __name__ == '__main__':
     main()
--- Test.py
+++ Test.py
@@ -1,31 +1,3 @@
-import serial
-
 import credentials
 
-# Open the serial port
-ser = serial.Serial('COM6', 9600)
-
-# Read and print the serial data
-while True:
-    data = ser.readline().decode().strip()
-    # print(data)
-    line = data.split(',')
-    flame_value = line[0].split(':')[1]
-    smoke_value = line[1].split(':')[1]
-    # print(Flame, Smoke)
-    # ip_address = os.popen('''hostname -I''').readline().replace('\n', '').replace(',', '.')[:-1]
-    # mac_address = os.popen('''type /sys/class/net/*/address''').readline().replace('\n', '').replace(',', '.')
-    ip_address = credentials.get_network_info()[0]
-    mac_address = credentials.get_network_info()[1] 
-    # print(ip_address, mac_address, Flame, Smoke)
-    attributes = {
-        'ip_address': ip_address,
-        'macaddress': mac_address
-    }
-    telemetry = {
-        'flame_value': int(flame_value),
-        'smoke_value': int(smoke_value)
-    }
-
-# Close the serial port
-ser.close()
+print(credentials.ACCESS_TOKEN)
